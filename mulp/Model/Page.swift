259d737e84a5f10d36a35ae407c4d97176e7b68e
Christian Jimenez <cejim@Christians-MBP.home>
2021-06-21 12:43:53 -0400

    Beginning again 2.0

---
+++ mulp/Model/Page.swift
@@ -0,0 +1,20 @@
+//
+//  Page.swift
+//  mulp
+//
+//  Created by Christian Jimenez on 6/21/21.
+//
+
+
+import UIKit
+
+class Page {
+    var id:String = ""
+    var canvas: UIImage
+    init(id: String,canvas:UIImage) {
+        self.id = id
+        self.canvas = canvas
+        
+    }
+    
+}
--- mulp/ViewController.swift
+++ mulp/ViewController.swift
@@ -7,31 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var textDisplay: UILabel!
+class PageCell:UITableViewCell{
     
-    @IBAction func helloWorld(_ sender: Any) {
-        textDisplay.text = "Hello, World!"
-        print("Pressed Hello World button")
-    }
-    
-    @IBAction func goodbyeWorld(_ sender: Any) {
-        textDisplay.text = "Goodbye World"
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        textDisplay.text = "Zzz..."
-        // Do any additional setup after loading the view.
-        print("viewDidLoad() called")
-    }
+}
 
-    override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            print("viewWillAppear(_:) called")
-            view.backgroundColor = UIColor.red
-
+class MainViewController: UITableViewController {
+    var pages = [Page]()
+    required init?(coder aDecoder: NSCoder) {
         
-        }
+        super.init(coder: aDecoder)
+        
+    }
+    init(pages: [Page]){
+        self.pages = pages
+        self.pages.insert(Page(id: "1", canvas: UIImage(named: "TheNerves" )!), at: 0)
+       
+        super.init( style: UITableView.Style.plain)
+        
+       
+    }
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.pages.count
+    }
+        
+}
+extension UIImage{
+    
+    func widthRatio() -> Float {
+        return  Float(self.size.height / self.size.width)
+        
+    }
 }
 
 
