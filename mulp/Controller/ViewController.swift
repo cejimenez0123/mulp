8d9be78204e60c64e7ef2048c7386cef57035bd3
christian <christianjimenz0123@gmail.com>
2021-06-28 09:38:44 -0400

    reloaddata

--- mulp/Controller/ViewController.swift
+++ mulp/Controller/ViewController.swift
@@ -9,18 +9,24 @@ import UIKit
 
 
 class MainViewController: UITableViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    let imagePicker = UIImagePickerController()
+    
     var pages = [Page]()
     init(pages: [Page]){
         self.pages = pages
-        self.pages.insert(Page(id: "1", canvas: UIImage(named: "TheNerves" )!), at: 0)
+        self.pages.append(Page(id: "1", canvas: UIImage(named: "TheNerves" )!))
         
         super.init( style: UITableView.Style.plain)
         
-        
+    
+       
       
     }
+    override func viewWillAppear(_ animated: Bool) {
+        self.tableView.estimatedRowHeight = UITableView.automaticDimension
+        self.tableView.rowHeight = 500
+    }
     @IBAction func uploadPhoto(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
         imagePicker.allowsEditing = true
         imagePicker.delegate = self
         present(imagePicker,animated: true)
@@ -30,34 +36,42 @@ class MainViewController: UITableViewController,UIImagePickerControllerDelegate,
         super.init(coder: aDecoder)
         
     }
+
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            let page = Page(id: UUID().uuidString, canvas: image)
-            pages.insert(page, at: 0)
-            }
-        self.tableView.reloadData()
-            self.imagePicker.dismiss(animated: true, completion: nil)
-       
+       let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        let page = Page(id: UUID().uuidString, canvas: image)
+        pages.insert(page, at: 0)
+        tableView.reloadData()
+            picker.dismiss(animated: true, completion: nil)
+    
     }
+
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
             dismiss(animated: true)
         }
    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return pages.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let pageCell = tableView.dequeueReusableCell(withIdentifier: "PageTableViewCell",for: indexPath) as! PageTableViewCell
-        pageCell.imageView!.image = pages[indexPath.row].picture
+       
+        pageCell.pic.image = pages[indexPath.row].picture
 
-
+        self.tableView.reloadRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        
         
         
         return pageCell
     }
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
-    }
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return UITableView.automaticDimension
+//    }
+//    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+//
+//
+//        return 400
+//    }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return pages.count
--- mulp/ModelView/PageTableViewCell.swift
+++ mulp/ModelView/PageTableViewCell.swift
@@ -17,10 +17,6 @@ class PageTableViewCell: UITableViewCell{
         
         
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-//        self.imageView!.image = pict
-//        self.imageView!.translatesAutoresizingMaskIntoConstraints = false
-//
-//        NSLayoutConstraint.activate([self.imageView!.topAnchor.constraint(equalTo: self.topAnchor),self.bottomAnchor.constraint(equalTo: self.bottomAnchor),self.imageView!.rightAnchor.constraint(equalTo: self.rightAnchor),self.imageView!.leftAnchor.constraint(equalTo: self.leftAnchor)])
     }
     required init?(coder: NSCoder) {
         super.init(coder:  coder)
